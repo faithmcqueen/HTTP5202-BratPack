d0f9ae11a8d83691b68ddd462f5cb51645e4d88a
ese-omene <55164935+ese-omene@users.noreply.github.com>
2020-03-10 15:07:53 -0400

    updated nav links

--- BratPack/includes/nav.php
+++ BratPack/includes/nav.php
@@ -1,8 +1,8 @@
 
 <nav>
     <ul>
-  <li><a href="home.php">Home</a></li>
+  <li><a href="index.php">Home</a></li>
    <li><a href="admin.php">Admin</a></li>
-   <li><a href="parents.php">Parents</a></li>
+   <li><a href="public-login.php">Parents</a></li>
     </ul>
 </nav>
