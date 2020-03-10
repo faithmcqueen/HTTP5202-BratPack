ec493bcff8cd075cc69ec8498d02f98e469de375
susanboratynska <susan.boratynska@gmail.com>
2020-03-10 11:42:20 -0400

    Public interface and login

---
+++ public-interface.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>The Brat Pack Daycare System - Homepage</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+
+<body class="text-center">
+<div><h1>Welcome "Parent Name"</h1></div>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <div>
+                <h2>Children</h2>
+                <ul class="list-group">
+                    <li class="list-group-item">Child 1</li>
+                    <li class="list-group-item">Child 2</li>
+                </ul>
+            </div>
+
+            <div>
+                <h2>Reminders</h2>
+                <ul class="list-group">
+                    <li class="list-group-item">Reminder 1</li>
+                    <li class="list-group-item">Reminder 2</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Artwork</h2>
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="images/artwork/art-art-materials-artistic-arts-and-crafts-542556.jpg" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="images/artwork/closeup-photo-of-assorted-color-alphabets-1337387.jpg" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="..." alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div>
+                <h2>Weekly Schedule</h2>
+                <ul class="list-group">
+                    <ul class="list-group">
+                        <li class="list-group-item">Monday - Activity</li>
+                        <li class="list-group-item">Tuesday - Activity</li>
+                        <li class="list-group-item">Wednesday - Activity</li>
+                        <li class="list-group-item">Thursday - Activity</li>
+                        <li class="list-group-item">Friday - Activity</li>
+                    </ul>
+                </ul>
+            </div>
+            <div>
+                <h2>Weekly Menu</h2>
+                <ul class="list-group">
+                    <li class="list-group-item">Monday - </li>
+                    <li class="list-group-item">Tuesday - </li>
+                    <li class="list-group-item">Wednesday - </li>
+                    <li class="list-group-item">Thursday - </li>
+                    <li class="list-group-item">Friday - </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+
---
+++ public-login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>The Brat Pack Daycare System - Login Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+
+<body class="text-center">
+    <div id="login__form">
+        <form class="form-signin">
+            <img class="mb-4" src="images/Logo-BratPack.jpeg" alt="The Brat Pack Logo" width="250px" height="auto">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <span>New to The Brat Pack Daycare System? <a href="/">Sign up here.</a></span>
+        </form>
+    </div>
+</body>
+</html>
+
+
