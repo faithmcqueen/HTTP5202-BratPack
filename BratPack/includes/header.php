b5b8d8ca19b0261b8b96df55c598f76e4b239240
ese-omene <55164935+ese-omene@users.noreply.github.com>
2020-03-12 23:19:29 -0400

    css updates

--- BratPack/includes/header.php
+++ BratPack/includes/header.php
@@ -4,13 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/layout.css">
     <title>The Brat Pack :: DayCare System</title>
 </head>
 <body>
 <header>
 
-    <h1>The Brat Pack :: DayCare System </h1>
+
     <img id="logo" src="images/logo.jpeg">
   <?php include 'nav.php'?>
 </header>
