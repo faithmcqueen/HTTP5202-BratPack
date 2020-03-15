79a822b46112aad3e8630a8798a767d965ea21ef
susanboratynska <susan.boratynska@gmail.com>
2020-03-15 10:42:20 -0400

    Folders and updates to staff directory

--- BratPack/staffdirectory_add.php
+++ BratPack/staffdirectory_add.php
@@ -8,38 +8,40 @@ include "includes/header.php";
         <div class="form-group row">
             <label for="staff__firstname" class="col-sm-2 col-form-label">First Name:</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control-plaintext" id="staff__firstname">
+                <input type="text" class="form-control" id="staff__firstname">
             </div>
         </div>
         <div class="form-group row">
             <label for="staff__lastname" class="col-sm-2 col-form-label">Last Name:</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control-plaintext" id="staff__lastname">
+                <input type="text" class="form-control" id="staff__lastname">
             </div>
         </div>
         <div class="form-group row">
             <label for="staff__email" class="col-sm-2 col-form-label">Email:</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control-plaintext" id="staff__email">
+                <input type="text" class="form-control" id="staff__email">
             </div>
         </div>
         <div class="form-group row">
             <label for="staff__phone" class="col-sm-2 col-form-label">Phone:</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control-plaintext" id="staff__phone">
+                <input type="text" class="form-control" id="staff__phone">
             </div>
         </div>
         <div class="form-group row">
             <label for="staff_title" class="col-sm-2 col-form-label">Title:</label>
-            <select id="staff_title" class="col-sm-8">
-                <option selected>Please Select One</option>
-                <option>Daycare Personnel</option>
-                <option>Daycare Caretaker</option>
-            </select>
+            <div class="col-sm-6">
+                <select id="staff_title" class="form-control col-sm-6">
+                    <option selected>Please Select One</option>
+                    <option>Daycare Personnel</option>
+                    <option>Daycare Caretaker</option>
+                </select>
+            </div>
         </div>
         <div class="form-group row">
             <label for="staff_photo" class="col-sm-2 col-form-label">Photo:</label>
-            <div class="input-group mb-3 col-sm-8" style="padding: 0px">
+            <div class="input-group mb-3 col-sm-6" >
                 <div class="custom-file">
                     <input type="file" class="custom-file-input" id="staff_photo">
                     <label class="custom-file-label" for="staff_photo">Choose file</label>
@@ -49,6 +51,7 @@ include "includes/header.php";
                 </div>
             </div>
         </div>
+        <button class="btn" type="submit">Add New Staff Member</button>
     </form>
 </div>
 
